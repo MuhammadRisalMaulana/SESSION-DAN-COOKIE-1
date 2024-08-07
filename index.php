e48cd6509b4017a54a49afc071d401c40b517faf
MuhammadRisalMaulana <ichal.rr26@gmail.com>
2024-08-07 21:26:48 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    setcookie("nama", "VSGA",strtotime('+7 days'), '/');
+    echo "Cookies berhasil di setting";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Cookies</title>
+</head>
+<body>
+    <h1>Belajar Cookies</h1>
+    <p><a href="about.html">About</a></p>
+    <p><a href="contact.html">Contact</a></p>
+    <p><a href="register.html">Register</a></p>
+</body>
+</html>
